6ac3a29fa9d5d08cc52403dc53bfbe10e5ca1579
Alisharebyc <ledyglam555@gmail.com>
2022-08-05 05:16:02 +0600

    add project2

---
+++ project2/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = 3;
+int numberB = 9;
+int result = numberA + numberB;
+//Console.WriteLine(numberA + numberB); второй способ вывести сумму двух чисел.
+Console.WriteLine(result); //первый способ вывести сумму. положить сумму в переменную result
